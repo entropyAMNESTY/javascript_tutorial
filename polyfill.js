802638a94962b5f02bf628104bf7ea0db2df1899
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-07-05 11:51:32 +0200

    polyfill completed

--- polyfill.js
+++ polyfill.js
@@ -29,8 +29,7 @@ arr1.forEach((item, index) => {
 arr2;
 
 // Custom forEach-Polyfill
-
-Array.prototype.foarEach = function (callback, argThis) {
+Array.prototype.forEach = function (callback, argThis) {
     // check wether the callback is a function
     if (typeof callback !== 'function') {
         throw new TypeError(
@@ -38,11 +37,31 @@ Array.prototype.foarEach = function (callback, argThis) {
         );
     }
     //we can continue our code here
-    console.log('yay, we can continue');
+    let len = this.length;
+
+    // Loop through our Array
+    for (var i = 0; i < len; i++) {
+        //callback goes here | three arguments
+        // Array.prototype.forEach = function(callback, thisArg) {...}
+        // callback: I  --> item    II  --> index   III --> array
+        callback(this[i], i, this);
+    }
 };
 
 let arr = ['milk', 'honey', 'sugar'];
-arr.foarEach(function () {});
+arr.forEach(function (item, index, argThis) {
+    // console.log(item, index, argThis);
+    console.log(this);
+    // outside of the function the this keyword refers to the window object
+});
 
-// Array.prototype.forEach = function(callback, thisArg) {...}
-// callback: I  --> item    II  --> index   III --> array
+// Output --> milk 0 (3) ['milk', 'honey', 'sugar']
+
+//Standard forEach-Method to compare
+// This refers to the Object it is pointing to
+
+arrExample = ['dog', 'cat', 'mouse'];
+// forEach function, called as a method
+arrExample.forEach(callback(){})
+// callback --> this used in these brackets refers to the arrExample
+// when you have a function that is not called as a method, then that function will be a method of the WINDOW OBJECT
